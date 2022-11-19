36d28ef169aa8fd866213eb20f944e5168ee4e17
Gabriel Rodrigues <rsgabriel01@gmail.com>
2022-11-18 21:33:41 -0300

    capitulo aPI no back-end concluido

--- 3-Especializar/2-TrabalahndoComAPIs/2-APInoBack-End/index.js
+++ 3-Especializar/2-TrabalahndoComAPIs/2-APInoBack-End/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const axios = require('axios')
 
 const app = express()
 
 app.listen('3000')
+
+//middleware
+app.use(express.json())
 
 console.log('Servidor em execução...')
 
@@ -14,11 +18,34 @@ console.log('Servidor em execução...')
 //   res.send('Hello Sobre')
 // })
 
-//middleware
-app.use(express.json())
+// app.route('/').post((req, res) => {
+//   console.log(req.body)
 
-app.route('/').post((req, res) => {
-  console.log(req.body)
+//   res.send(req.body)
+// })
 
-  res.send(req.body)
+// let author = 'Gabriel'
+
+// app.route('/').get((req, res) => {
+//   res.send({ author })
+// })
+
+// app.route('/').put((req, res) => {
+//   let lastAuthor = author
+//   author = req.body.newAuthor
+//   let newAuthor = author
+
+//   res.send({ lastAuthor, newAuthor })
+// })
+
+let author = 'Gabriel'
+
+app.route('/:identificador').delete((req, res) => {
+  author = ''
+
+  res.send(`ID: ${req.params.identificador} apagado!`)
 })
+
+// app.route('/').get((req, res) => {
+//   axios.get()
+// })
